d3fd1f06d519a42e4657c168e5ad44495371176d
luisrodriguezmerino <101235525+luisrodriguezmerino@users.noreply.github.com>
2022-04-29 17:33:59 -0500

    Delete Commits

--- php/Conexion.php
+++ php/Conexion.php
@@ -1,18 +1,4 @@
 <?php
-/*
-$servername = "localhost";
-$username = "TeemoPatriarca";
-$password = "soyleyenda666";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";*/
-
 $usuario  = "UserSuplos";
 $password = "215233";
 $servidor = "localhost";
